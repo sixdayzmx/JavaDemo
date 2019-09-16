3ea7e3f5c960ac0dbf95a88a0aa4489d47a4e79c
sixdayzmx <sixdayzmx@163.com>
2019-09-16 11:56:04 +0800

    十次方项目第二次提交： 已经学到第八天,熔断器阶段

--- tensquare_base/src/main/java/com/zmx/base/controller/LableController.java
+++ tensquare_base/src/main/java/com/zmx/base/controller/LableController.java
@@ -62,12 +62,13 @@ public class LableController {
     @RequestMapping(value = "/search",method=RequestMethod.POST)
     public Result findSearch(@RequestBody Label label){
         List<Label> list=labelService.findSearch(label);
-        return new Result(true,StatusCode.OK,"查询成功",list);
+        return new Result(true,StatusCode.OK,"查 询成功",list);
     }
 
     @RequestMapping(value = "/search/{page}/{size}",method = RequestMethod.POST)
     public Result pageQuery(@RequestBody Label label,@PathVariable int page,@PathVariable int size){
         Page<Label> pageData=labelService.pageQuery(label,page,size);
+        
         return new Result(true,StatusCode.OK,"查询成功",new PageResult<Label>(pageData.getTotalElements(),pageData.getContent()));
     }
 }
